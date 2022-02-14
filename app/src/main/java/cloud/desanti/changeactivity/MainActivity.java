d80434574b25063cab6e4c194dad69ea79438feb
neokyr <p00riel.kyrillin@gmail.com>
2022-02-14 15:47:05 +0100

    Dernière version

--- app/src/main/java/cloud/desanti/changeactivity/MainActivity.java
+++ app/src/main/java/cloud/desanti/changeactivity/MainActivity.java
@@ -1,17 +1,24 @@
 package cloud.desanti.changeactivity;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
     private EditText etMessage;
+    private int idActivity2 = 77;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         etMessage = findViewById(R.id.etMessage);
-        //Intent act2 = new Intent(getApplicationContext(), SecondActivity.class);
-        Intent act2 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.qwant.fr"));
 
 
         getBaseContext();
         btn = findViewById(R.id.btnAct2);
 
         btn.setOnClickListener(view -> {
-
+            Intent act2 = new Intent(getApplicationContext(), SecondActivity.class);
             act2.putExtra("message", etMessage.getText().toString());
-            startActivity(act2);
+            startActivityForResult(act2,idActivity2);
+
 
         });
 
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == idActivity2 && resultCode == 6) {
+            TextView vv = findViewById(R.id.textView2);
+
+            vv.setText(requestCode + " a répondu " + resultCode + " et message : "+ data.getSerializableExtra("dataRe"));
+        }
     }
 }
--- app/src/main/java/cloud/desanti/changeactivity/SecondActivity.java
+++ app/src/main/java/cloud/desanti/changeactivity/SecondActivity.java
@@ -2,7 +2,11 @@ package cloud.desanti.changeactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -13,8 +17,26 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         TextView txt = findViewById(R.id.txtReceiver);
-        Bundle extra  = getIntent().getExtras();
+        Bundle extra = getIntent().getExtras();
 
         txt.setText((String) extra.getSerializable("message"));
+
+        Button btn = findViewById(R.id.btnReponse);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ia2 = new Intent();
+
+                EditText et = findViewById(R.id.edReponse);
+                ia2.putExtra("dataRe", et.getText().toString() );
+
+                setResult(6, ia2);
+
+                finish();
+            }
+        });
     }
+
+
 }
